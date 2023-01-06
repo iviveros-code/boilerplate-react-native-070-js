eb6e3b7210c6ff5406cd457169483a8e3efad436
Ivan Viveros <ivanviveros@MacBook-Pro-de-Ivan-4.local>
2023-01-06 17:41:28 -0300

    custom splash

--- template/App.js
+++ template/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from '@navigation';
 
@@ -7,17 +7,36 @@ import {Provider as ReduxProvider} from 'react-redux';
 import store from './src/redux/store';
 import {theme} from '@theme';
 
+import {WithSplashScreen} from '@components/Splash';
+
 import {Provider as PaperProvider} from 'react-native-paper';
 
 const App = () => {
+  const storeSplash = useRef(undefined);
+  const queryClient = useRef(undefined);
+
+  const [isAppReady, setIsAppReady] = useState(false);
+
+  const initializeApp = async () => {
+    setTimeout(() => {
+      setIsAppReady(true);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    initializeApp();
+  }, []);
+
   return (
-    <ReduxProvider store={store}>
-      <PaperProvider theme={theme}>
-        <NavigationContainer>
-          <Navigation />
-        </NavigationContainer>
-      </PaperProvider>
-    </ReduxProvider>
+    <WithSplashScreen isAppReady={isAppReady}>
+      <ReduxProvider store={store}>
+        <PaperProvider theme={theme}>
+          <NavigationContainer>
+            <Navigation />
+          </NavigationContainer>
+        </PaperProvider>
+      </ReduxProvider>
+    </WithSplashScreen>
   );
 };
 
--- template/ios/template/AppDelegate.mm
+++ template/ios/template/AppDelegate.mm
@@ -46,11 +46,13 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   NSDictionary *initProps = [self prepareInitialProps];
   UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"template", initProps);
 
-  if (@available(iOS 13.0, *)) {
-    rootView.backgroundColor = [UIColor systemBackgroundColor];
-  } else {
-    rootView.backgroundColor = [UIColor whiteColor];
-  }
+  // if (@available(iOS 13.0, *)) {
+  //   rootView.backgroundColor = [UIColor systemBackgroundColor];
+  // } else {
+  //   rootView.backgroundColor = [UIColor whiteColor];
+  // }
+  rootView.backgroundColor = [UIColor colorWithRed:0.38824 green:0.43137 blue:0.44706 alpha:1.00000];
+
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
