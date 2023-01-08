bec141ad36173795da94b69a0f5e701a9a30f72c
Ivan Viveros <ivanviveros@MacBook-Pro-de-Ivan-4.local>
2023-01-08 19:06:36 -0300

    launchscreen on ios

--- template/App.js
+++ template/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from '@navigation';
 
@@ -7,36 +7,17 @@ import {Provider as ReduxProvider} from 'react-redux';
 import store from './src/redux/store';
 import {theme} from '@theme';
 
-import {WithSplashScreen} from '@components/Splash';
-
 import {Provider as PaperProvider} from 'react-native-paper';
 
 const App = () => {
-  const storeSplash = useRef(undefined);
-  const queryClient = useRef(undefined);
-
-  const [isAppReady, setIsAppReady] = useState(false);
-
-  const initializeApp = async () => {
-    setTimeout(() => {
-      setIsAppReady(true);
-    }, 2000);
-  };
-
-  useEffect(() => {
-    initializeApp();
-  }, []);
-
   return (
-    <WithSplashScreen isAppReady={isAppReady}>
-      <ReduxProvider store={store}>
-        <PaperProvider theme={theme}>
-          <NavigationContainer>
-            <Navigation />
-          </NavigationContainer>
-        </PaperProvider>
-      </ReduxProvider>
-    </WithSplashScreen>
+    <ReduxProvider store={store}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer>
+          <Navigation />
+        </NavigationContainer>
+      </PaperProvider>
+    </ReduxProvider>
   );
 };
 
